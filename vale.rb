c31475b159623045f24be905ef21d7f207e1fbb6
Joseph Kato <joseph@jdkato.io>
2017-04-18 10:37:57 -0700

    v0.4.1

--- vale.rb
+++ vale.rb
@@ -1,9 +1,9 @@
 class Vale < Formula
   desc "A customizable, syntax-aware linter for prose."
   homepage "https://github.com/ValeLint/vale"
-  url "https://github.com/ValeLint/vale/releases/download/v0.4.0/macOS-64bit.tar.gz"
-  version "v0.4.0"
-  sha256 "609c9f1cb5a9d733fe9319755e97699753ee9466c3e7ccb6f4ac8c7f876a474e"
+  url "https://github.com/ValeLint/vale/releases/download/v0.4.1/macOS-64bit.tar.gz"
+  version "v0.4.1"
+  sha256 "9fce2737671d3ba02d2936b6adffbedad528bc38f982d9ab4a9fce5ae46ad9db"
 
   def install
     bin.install "vale"
