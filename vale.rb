981550c7d3c2eb714fdf6ddbf3173da0961a3474
Joseph Kato <joseph@jdkato.io>
2017-05-28 14:52:43 -0700

    Fix version again

--- vale.rb
+++ vale.rb
@@ -4,10 +4,10 @@ class Vale < Formula
   version "0.7.0"
 
   if OS.mac?
-    url "https://github.com/ValeLint/vale/releases/download/#{version}/vale_macOS_64-bit.tar.gz"
+    url "https://github.com/ValeLint/vale/releases/download/#{version}/vale_#{version}_macOS_64-bit.tar.gz"
     sha256 "1a946d23ce26649bbaab2635f52192746ae6ae86a9ef9ab33661773f42c640ad"
   elsif OS.linux?
-    url "https://github.com/ValeLint/vale/releases/download/#{version}/vale_Linux_64-bit.tar.gz"
+    url "https://github.com/ValeLint/vale/releases/download/#{version}/vale_#{version}_Linux_64-bit.tar.gz"
     sha256 "96795b625dd622d4af523c123c6adaa5164ca521aa8041d909eb5fae12ff180e"
   end
 
