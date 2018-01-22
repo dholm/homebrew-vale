6b61849fa8fb6978c521799052245b6340e00f03
goreleaserbot <goreleaser@carlosbecker.com>
2018-01-21 22:38:43 -0800

    vale version 0.9.1

--- vale.rb
+++ vale.rb
@@ -1,17 +1,15 @@
 class Vale < Formula
   desc "A customizable, syntax-aware linter for prose."
   homepage "https://github.com/ValeLint/vale"
-  version "0.9.0"
-
-  if OS.mac?
-    url "https://github.com/ValeLint/vale/releases/download/#{version}/vale_#{version}_macOS_64-bit.tar.gz"
-    sha256 "ed45c14e1f3963157efa6af38d857b63d5274296ee72fb8986c9d1ce8eea9a87"
-  elsif OS.linux?
-    url "https://github.com/ValeLint/vale/releases/download/#{version}/vale_#{version}_Linux_64-bit.tar.gz"
-    sha256 "d012dc5654ff5f3a4bf6fcfa9a3bfa1457d07d419cbed14bef389d4b3ce1c8ba"
-  end
+  url "https://github.com/ValeLint/vale/releases/download/0.9.1/vale_0.9.1_macOS_64-bit.tar.gz"
+  version "0.9.1"
+  sha256 "2297b19f9b4f4e8a54ba1383bda2659e5c91025087a702ced995d04870a2b18c"
 
   def install
     bin.install "vale"
+  end
+
+  test do
+    
   end
 end
