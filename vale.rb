32210062d683350a0fa528b0ba23b3dcbd1675bc
goreleaserbot <goreleaser@carlosbecker.com>
2018-03-30 17:20:50 -0700

    Brew formula update for vale version v0.11.0

--- vale.rb
+++ vale.rb
@@ -1,9 +1,9 @@
 class Vale < Formula
   desc "A customizable, syntax-aware linter for prose."
   homepage "https://github.com/ValeLint/vale"
-  url "https://github.com/ValeLint/vale/releases/download/0.10.1/vale_0.10.1_macOS_64-bit.tar.gz"
-  version "0.10.1"
-  sha256 "97fc2a4fcfbdfebec03a3f5ca65c8c0620accbaae74178179d86d0db222a1e79"
+  url "https://github.com/ValeLint/vale/releases/download/v0.11.0/vale_0.11.0_macOS_64-bit.tar.gz"
+  version "0.11.0"
+  sha256 "6bf533bcb896d3c9d1c34d102358b86fd8e0f43d6a776f7103d4d61256140547"
 
   def install
     bin.install "vale"
