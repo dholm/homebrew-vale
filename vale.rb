a32524bbe30f9614322aa023f4ab1eaef0498c21
goreleaserbot <goreleaser@carlosbecker.com>
2018-08-29 18:03:29 -0700

    Brew formula update for vale version v1.0.3

--- vale.rb
+++ vale.rb
@@ -1,9 +1,9 @@
 class Vale < Formula
   desc "A customizable, syntax-aware linter for prose."
   homepage "https://github.com/errata-ai/vale"
-  url "https://github.com/errata-ai/vale/releases/download/v1.0.2/vale_1.0.2_macOS_64-bit.tar.gz"
-  version "1.0.2"
-  sha256 "1897c3214b29e6f51358cb632fe21d9a0c3bfeeb189566be5933b54ee6f4f2ee"
+  url "https://github.com/errata-ai/vale/releases/download/v1.0.3/vale_1.0.3_macOS_64-bit.tar.gz"
+  version "1.0.3"
+  sha256 "f0d6f6957b34037e8883844ca684d6805b5bc8d2baf488e2268883449149e225"
 
   def install
     bin.install "vale"
