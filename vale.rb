d7d306d2a6fea1de9c5c61f591aa7e0cb5e2e2e4
David Holm <david.holm@ericsson.com>
2019-01-09 08:49:14 +0100

    vale: Re-adds support for Linuxbrew

--- vale.rb
+++ vale.rb
@@ -1,9 +1,14 @@
 class Vale < Formula
   desc "A customizable, syntax-aware linter for prose."
   homepage "https://github.com/errata-ai/vale"
-  url "https://github.com/errata-ai/vale/releases/download/v1.2.7/vale_1.2.7_macOS_64-bit.tar.gz"
   version "1.2.7"
-  sha256 "13b68c2f1ec9e6aa35bf979caf9687e25337534cb2d76122865f3faf56ac9080"
+  if OS.mac?
+    url "https://github.com/errata-ai/vale/releases/download/v#{version}/vale_#{version}_macOS_64-bit.tar.gz"
+    sha256 "13b68c2f1ec9e6aa35bf979caf9687e25337534cb2d76122865f3faf56ac9080"
+  elsif OS.linux?
+    url "https://github.com/errata-ai/vale/releases/download/v#{version}/vale_#{version}_Linux_64-bit.tar.gz"
+    sha256 "cbd16df9c63bbd6ea60cbfd1bd495f1a38bb649d0fb3dd80605d0dac8b7358a7"
+  end
 
   def install
     bin.install "vale"
